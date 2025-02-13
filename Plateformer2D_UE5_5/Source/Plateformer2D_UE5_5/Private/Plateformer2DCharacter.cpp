01f11578120ae8f3d5575e4036e6ca7f091d5c4b
Dejan Popov <dekipopica92@gmail.com>
2025-02-13 20:53:40 +0100

    Setting camera to player

--- Plateformer2D_UE5_5/Source/Plateformer2D_UE5_5/Private/Plateformer2DCharacter.cpp
+++ Plateformer2D_UE5_5/Source/Plateformer2D_UE5_5/Private/Plateformer2DCharacter.cpp
@@ -3,3 +3,22 @@
 
 #include "Plateformer2DCharacter.h"
 
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+APlateformer2DCharacter::APlateformer2DCharacter()
+{
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArmComponent->SetupAttachment(RootComponent);
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	CameraComponent->SetupAttachment(SpringArmComponent);
+
+	SceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Scene"));
+	SceneComponent->SetupAttachment(RootComponent);
+}
+
+void APlateformer2DCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
--- Plateformer2D_UE5_5/Source/Plateformer2D_UE5_5/Public/Plateformer2DCharacter.h
+++ Plateformer2D_UE5_5/Source/Plateformer2D_UE5_5/Public/Plateformer2DCharacter.h
@@ -13,5 +13,24 @@ UCLASS()
 class PLATEFORMER2D_UE5_5_API APlateformer2DCharacter : public APaperCharacter
 {
 	GENERATED_BODY()
-	
+
+	// Constructor of the player
+	APlateformer2DCharacter();
+
+public:
+
+	virtual void BeginPlay() override;
+
+private:
+
+	// This component tries to maintain its children at fixed distance from the parent
+	// It is going to be attached to the camera
+	UPROPERTY(VisibleAnywhere, Category = "Plateformer2D|Actor Component")
+	class USpringArmComponent* SpringArmComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = "Plateformer2D|Actor Component")
+	class UCameraComponent* CameraComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = "Plateformer2D|Actor Component")
+	USceneComponent* SceneComponent;
 };
